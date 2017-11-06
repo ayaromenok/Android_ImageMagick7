91fddca6b0f7a051da4d90b0e1b01960d1b00e2b
Andrey Yaromenok <ayaromenok@gmail.com>
2017-11-06 07:49:14 +0100

    #14 build scripts updated to cleanup previous build correctly

--- jni/gccBuild.sh
+++ jni/gccBuild.sh
@@ -1,6 +1,23 @@
 #/bin/sh
+cd  ..
+rm -f -r obj
+rm -f -r libs.gcc
+rm -f -r libs
+cd ./jni
+
 rm -f ./Application.mk
 ln -s ./Application.mk.gcc ./Application.mk
-rm -s ./Android.mk
+rm -f ./Android.mk
 ln -s ./Android.mk.openmp ./Android.mk
+
+cd ./ImageMagick-7.0.5-2/MagickCore
+rm -f magick-baseconfig.h
+ln -s magick-baseconfig.h.openmp magick-baseconfig.h
+cd ../..
+
 $ANDROID_SDK_PATH/ndk-bundle/ndk-build -j 4
+
+cd ..
+mv libs libs.gcc
+ln -s libs.gcc libs
+cd ./jni
